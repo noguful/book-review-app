bc64297035b8c1a0dd7868cfaf0eb565355c76e3
noguful <noguchi.shin1@gmail.com>
2023-12-15 11:30:14 +0900

    Cypress テスト用のフォームを作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [error, setError] = useState(false);
+  const inputRef = useRef(null);
+  const handleClick = () => {
+    const inputValue = inputRef.current.value;
+    setName(inputValue);
+    if (!inputValue) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="greeting">
+        <div className="greeting-form">
+          <input type="text" className="greeting-input" placeholder="名前を入力してください" ref={inputRef} />
+          <button type="button" className="greeting-button" onClick={handleClick}>Greeting</button>
+        </div>
+        <div className="greeting-contents">
+          {name && <p className="greeting-body">こんにちは！{name}さん！</p>}
+          {error && <p className="greeting-error">名前が空です！</p>}
+          {console.log(typeof inputValue)}
+        </div>
+      </div>
     </div>
   );
 }
