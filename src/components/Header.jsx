626f36703fe918aa026422f3776ac8aa935258b6
noguful <noguchi.shin1@gmail.com>
2023-12-20 21:49:57 +0900

    headerのスタイル調整

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './header.scss'
 
 export const Header = () => {
   return (
     <header className="header">
-      <h1>書籍レビュー</h1>
+      <h1 className="header__title">書籍レビュー</h1>
     </header>
   );
 };
