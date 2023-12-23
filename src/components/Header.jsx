19d1816a8b871dc9c038667046424568dc2267ce
noguful <noguchi.shin1@gmail.com>
2023-12-23 22:00:05 +0900

    ログインしているかしていないかでheaderの情報を切り替える

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,30 +3,50 @@ import { useCookies } from 'react-cookie';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { signOut } from '../authSlice';
+import axios from 'axios';
+import useSWR from 'swr';
+import { url } from '../const';
+import { Link } from 'react-router-dom';
 import './header.scss'
 
 export const Header = () => {
   const auth = useSelector((state) => state.auth.isSignIn);
   const dispatch = useDispatch();
   const history = useNavigate();
-  // eslint-disable-next-line no-unused-vars
-  const [cookies, setCookie, removeCookie] = useCookies();
+  const [cookies, , removeCookie] = useCookies();
   const handleSignOut = () => {
     dispatch(signOut());
     removeCookie('token');
     history('/login');
   };
 
+  const fetcher = (url) =>
+    axios
+      .get(url, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((res) => res.data);
+  const { data } = useSWR( auth ? `${url}/users` : null, fetcher)
+
   return (
     <header className="header">
       <h1 className="header__title">書籍レビュー</h1>
-      {auth ? (
-        <button onClick={handleSignOut} className="sign-out-button">
-          サインアウト
-        </button>
-      ) : (
-        <></>
-      )}
+      <div className="header__control">
+        {auth ? (
+          <>
+            <p className="header__user">{data?.name}さん</p>
+            <button onClick={handleSignOut} className="button button--white">
+              ログアウト
+            </button>
+          </>
+        ) : (
+          <p>
+            <Link to="/login" className="button button--white">ログイン画面へ</Link>
+          </p>
+        )}
+      </div>
     </header>
   );
 };
