eadbec3df1bbd95f7c8d2c9c035a6560dd4027d5
noguful <noguchi.shin1@gmail.com>
2023-12-23 14:38:57 +0900

    Headerにログインとログアウトの処理を追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from '../authSlice';
 import './header.scss'
 
 export const Header = () => {
+  const auth = useSelector((state) => state.auth.isSignIn);
+  const dispatch = useDispatch();
+  const history = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const handleSignOut = () => {
+    dispatch(signOut());
+    removeCookie('token');
+    history('/login');
+  };
+
   return (
     <header className="header">
       <h1 className="header__title">書籍レビュー</h1>
+      {auth ? (
+        <button onClick={handleSignOut} className="sign-out-button">
+          サインアウト
+        </button>
+      ) : (
+        <></>
+      )}
     </header>
   );
 };
